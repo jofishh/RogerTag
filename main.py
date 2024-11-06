1f1bd74c9a6da6980f9df712d507e5949524a6dc
Joseph Mudge <jofish@mail.com>
2024-11-06 16:17:27 +0000

    Gameloop

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello world")
+import pygame as pg
+import math, sys
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.width = game_settings.width
+        self.height = game_settings.height
+        self.screen = pg.display.set_mode((self.width, self.height))
+        pg.display.set_caption("RogTag")
+        self.running = True
+        self.clock = pg.time.Clock()
+
+    def handle_events(self): 
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+
+    def run(self):
+        while self.running:
+            self.handle_events()
+            self.screen.fill((0, 0, 0))
+            pg.display.flip()
+            self.clock.tick(game_settings.FPS)
+        pg.quit()
+
+game = Game()
+game.run()
